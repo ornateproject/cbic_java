ebad6627da954b51210f8dc67bf0fab91b65fd25
user <iamrakeshit273@gmail.com>
2024-06-05 15:23:55 +0530

    total score gst3 updated

--- cbicApi/src/main/java/com/ornates/cbic/controller/TotalScoreController.java
+++ cbicApi/src/main/java/com/ornates/cbic/controller/TotalScoreController.java
@@ -1,4 +1,5 @@
 package com.ornates.cbic.controller;
+
 import com.ornates.cbic.dao.pool.JDBCConnection;
 import com.ornates.cbic.dao.result.GetExecutionSQL;
 import com.ornates.cbic.model.response.TotalScore;
@@ -18,92 +19,92 @@ import java.util.List;
 @CrossOrigin
 @RequestMapping("/cbic/t_score")
 @Controller
-
 public class TotalScoreController {
-	 @ResponseBody
-	    @RequestMapping(value = "/")
-	    public String home() {
-	        return "its working api";
-	    }
+	@ResponseBody
+	@RequestMapping(value = "/")
+	public String home() {
+		return "its working api";
+	}
 
-	    /*
-	     * Date: june 4, 2024
-	     * Purpose: This methods for performing check the database  .
-	     */
-	    @ResponseBody
-	    @RequestMapping(value = "/db_check")
-	    public String bdTest() {
+	/*
+	 * Date: june 4, 2024
+	 * Purpose: This methods for performing check the database  .
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/db_check")
+	public String bdTest() {
 
-	        //Connection done
-	        Connection con = JDBCConnection.getTNConnection();
-	        System.out.println("Connection :"+con);
-	        return "its working api";
-	    }
-	    @ResponseBody
-	    @RequestMapping(value = "/scrutiny/assessment")
-	    //  http://localhost:8080/CbicAPI/cbic/t_score/scrutiny/assessment?month_date=2023-04-01
-	    public Object scrutinyAssessment(@RequestParam String month_date) {
-	        List<TotalScore> allGstaList = new ArrayList<>();
-	        Connection con = null;
-	        ResultSet rsGst14aa = null;
-	        try {
-	            String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+		//Connection done
+		Connection con = JDBCConnection.getTNConnection();
+		System.out.println("Connection :"+con);
+		return "its working api";
+	}
+	@ResponseBody
+	@RequestMapping(value = "/scrutiny/assessment")
+	//  http://localhost:8080/CbicAPI/cbic/t_score/scrutiny/assessment?month_date=2023-04-01
+	public Object scrutinyAssessment(@RequestParam String month_date) {
+		List<TotalScore> allGstaList = new ArrayList<>();
+		Connection con = null;
+		ResultSet rsGst14aa = null;
+		try {
+			String prev_month_new = DateCalculate.getPreviousMonth(month_date);
 
-	            String query_assessment = "WITH cte AS (\n" +
-	                    "                    SELECT\n" +
-	                    "                    zc.ZONE_NAME,\n" +
-	                    "                    cc.ZONE_CODE,\n" +
-	                    "                    (SUM(CASE WHEN 14c.MM_YYYY = '" + month_date + "' THEN 14c.SCRUTINIZED_ASMT_10 ELSE 0 END) +\n" +
-	                    "                            SUM(CASE WHEN 14c.MM_YYYY = '" + month_date + "' THEN 14c.SCRUTINIZED_DISCRIPANCY_FOUND ELSE 0 END)) /\n" +
-	                    "                            (SUM(CASE WHEN 14c.MM_YYYY = '" + month_date + "' THEN 14c.RETURN_SCRUTINY ELSE 0 END) +\n" +
-	                    "                                    SUM(CASE WHEN 14c.MM_YYYY = '" + prev_month_new + "' THEN 14c.CLOSING_NO ELSE 0 END)) AS hgf\n" +
-	                    "                    FROM mis_gst_commcode AS cc\n" +
-	                    "                    RIGHT JOIN mis_dggst_gst_scr_1 AS 14c ON cc.COMM_CODE = 14c.COMM_CODE\n" +
-	                    "                    LEFT JOIN mis_gst_zonecode AS zc ON zc.ZONE_CODE = cc.ZONE_CODE\n" +
-	                    "                    WHERE 14c.MM_YYYY IN ('" + month_date + "', '" + prev_month_new + "')\n" +
-	                    "                    GROUP BY zc.ZONE_NAME, cc.ZONE_CODE\n" +
-	                    "\n" +
-	                    "                    UNION ALL\n" +
-	                    "\n" +
-	                    "                    SELECT\n" +
-	                    "                    zc.ZONE_NAME,\n" +
-	                    "                    cc.ZONE_CODE,\n" +
-	                    "                    SUM(14c.amount_recovered_penalty) / SUM(14c.tax_liability_detectect) AS hgf\n" +
-	                    "                    FROM mis_gst_commcode AS cc\n" +
-	                    "                    RIGHT JOIN mis_dggst_gst_scr_2a AS 14c ON cc.COMM_CODE = 14c.COMM_CODE\n" +
-	                    "                    LEFT JOIN mis_gst_zonecode AS zc ON zc.ZONE_CODE = cc.ZONE_CODE\n" +
-	                    "                    WHERE 14c.MM_YYYY = '" + month_date + "'\n" +
-	                    "                    GROUP BY zc.ZONE_NAME, cc.ZONE_CODE\n" +
-	                    "            )\n" +
-	                    "            SELECT ZONE_NAME, ZONE_CODE, AVG(hgf) AS total_score\n" +
-	                    "            FROM cte\n" +
-	                    "            GROUP BY ZONE_NAME, ZONE_CODE;";
+			String query_assessment = "WITH cte AS (\n" +
+					"                        SELECT \n" +
+					"                            zc.ZONE_NAME, \n" +
+					"                            cc.ZONE_CODE, \n" +
+					"                            (SUM(CASE WHEN 14c.MM_YYYY = '" + month_date + "' THEN 14c.SCRUTINIZED_ASMT_10 ELSE 0 END) + \n" +
+					"                             SUM(CASE WHEN 14c.MM_YYYY = '" + month_date + "' THEN 14c.SCRUTINIZED_DISCRIPANCY_FOUND ELSE 0 END)) /\n" +
+					"                            (SUM(CASE WHEN 14c.MM_YYYY = '" + month_date + "' THEN 14c.RETURN_SCRUTINY ELSE 0 END) + \n" +
+					"                             SUM(CASE WHEN 14c.MM_YYYY = '" + prev_month_new + "' THEN 14c.CLOSING_NO ELSE 0 END)) AS hgf\n" +
+					"                        FROM mis_gst_commcode AS cc \n" +
+					"                        RIGHT JOIN mis_dggst_gst_scr_1 AS 14c ON cc.COMM_CODE = 14c.COMM_CODE \n" +
+					"                        LEFT JOIN mis_gst_zonecode AS zc ON zc.ZONE_CODE = cc.ZONE_CODE \n" +
+					"                        WHERE 14c.MM_YYYY IN ('" + month_date + "', '" + prev_month_new + "') \n" +
+					"                        GROUP BY zc.ZONE_NAME, cc.ZONE_CODE\n" +
+					"\n" +
+					"                        UNION ALL\n" +
+					"\n" +
+					"                        SELECT \n" +
+					"                            zc.ZONE_NAME, \n" +
+					"                            cc.ZONE_CODE, \n" +
+					"                            SUM(14c.amount_recovered_penalty) / SUM(14c.tax_liability_detectect) AS hgf\n" +
+					"                        FROM mis_gst_commcode AS cc\n" +
+					"                        RIGHT JOIN mis_dggst_gst_scr_2a AS 14c ON cc.COMM_CODE = 14c.COMM_CODE\n" +
+					"                        LEFT JOIN mis_gst_zonecode AS zc ON zc.ZONE_CODE = cc.ZONE_CODE \n" +
+					"                        WHERE 14c.MM_YYYY = '" + month_date + "'\n" +
+					"                        GROUP BY zc.ZONE_NAME, cc.ZONE_CODE\n" +
+					"                    )\n" +
+					"                    SELECT ROW_NUMBER() OVER (ORDER BY AVG(hgf) DESC) AS z_rank,\n" +
+					"                           ZONE_NAME, \n" +
+					"                           ZONE_CODE, \n" +
+					"                           AVG(hgf) AS total_score\n" +
+					"                    FROM cte \n" +
+					"                    GROUP BY ZONE_NAME, ZONE_CODE \n" +
+					"                    ORDER BY total_score DESC;";
 
+			rsGst14aa= GetExecutionSQL.getResult(query_assessment);
 
-	           // con = JDBCConnection.getTNConnection();
-	           // rsGst14aa = GetExecutionSQL.getResult(con, query_assessment);
-	             rsGst14aa= GetExecutionSQL.getResult(query_assessment);
+			while (rsGst14aa.next()) {
+				double total_score = rsGst14aa.getDouble("total_score");
+				Integer Zonal_rank = rsGst14aa.getInt("z_rank");
+				String zoneName = rsGst14aa.getString("ZONE_NAME");
+				String commName = "ALL";
 
-	            while (rsGst14aa != null && rsGst14aa.next()) {
-//	                double total_score_1 = rsGst14aa.getDouble("total_score_1");
-//	                double total_score_2 = rsGst14aa.getDouble("total_score_2");
-	                double total_score = rsGst14aa.getDouble("total_score");
+				TotalScore totalScore = new TotalScore(zoneName, commName, total_score, Zonal_rank);
+				allGstaList.add(totalScore);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rsGst14aa != null) rsGst14aa.close();
+				if (con != null) con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return allGstaList;
 
-	                TotalScore totalScore = new TotalScore(total_score);
-	                allGstaList.add(totalScore);
-	            }
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        } finally {
-	            try {
-	                if (rsGst14aa != null) rsGst14aa.close();
-	                if (con != null) con.close();
-	            } catch (SQLException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	        return allGstaList;
-
-	    }
-
+	}
 }
--- cbicApi/src/main/java/com/ornates/cbic/model/response/TotalScore.java
+++ cbicApi/src/main/java/com/ornates/cbic/model/response/TotalScore.java
@@ -1,18 +1,47 @@
 package com.ornates.cbic.model.response;
 
 public class TotalScore {
-	private  double totalScore;
+    private String zoneName;
+    private  String commName;
+    private  double totalScore;
+    private  Integer Zonal_rank;
 
-    public void setTotalScore(double totalScore) {
+    public TotalScore(String zoneName, String commName, double totalScore, Integer zonal_rank) {
+        this.zoneName = zoneName;
+        this.commName = commName;
         this.totalScore = totalScore;
+        Zonal_rank = zonal_rank;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public String getCommName() {
+        return commName;
+    }
+
+    public void setCommName(String commName) {
+        this.commName = commName;
     }
 
     public double getTotalScore() {
         return totalScore;
     }
 
-    public TotalScore(double totalScore) {
+    public void setTotalScore(double totalScore) {
         this.totalScore = totalScore;
     }
 
+    public Integer getZonal_rank() {
+        return Zonal_rank;
+    }
+
+    public void setZonal_rank(Integer zonal_rank) {
+        Zonal_rank = zonal_rank;
+    }
 }
