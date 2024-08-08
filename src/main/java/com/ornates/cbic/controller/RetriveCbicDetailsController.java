cf2c8fd9bceb13c95eedd3e3173a802ae302fa78
rakeshshit <iamrakeshit60@gmail.com>
2024-08-08 14:31:52 +0530

    sub_parameter wise gst11a updated as per new requirement

--- src/main/java/com/ornates/cbic/controller/RetriveCbicDetailsController.java
+++ src/main/java/com/ornates/cbic/controller/RetriveCbicDetailsController.java
@@ -5396,43 +5396,45 @@ public class RetriveCbicDetailsController {
             if (type.equalsIgnoreCase("zone")) {
                 String prev_month_new = DateCalculate.getPreviousMonth(month_date);
 
-                String queryTotalPending = "WITH cte AS (" +
-                        "SELECT zc.ZONE_NAME, zc.ZONE_CODE, SUM(DEPARTMENT_DISPOSAL_NO + TAXPAYER_DISPOSAL_NO) AS col10 " +
-                        "FROM mis_dla_gst_lgl_1 11a " +
-                        "LEFT JOIN mis_gst_commcode cc ON 11a.COMM_CODE = cc.COMM_CODE " +
-                        "LEFT JOIN mis_gst_zonecode zc ON cc.ZONE_CODE = zc.ZONE_CODE " +
-                        "WHERE 11a.MM_YYYY = '" + month_date + "' AND FORUM_CODE = 6 " +
-                        "GROUP BY zc.ZONE_CODE), " +
-                        "cte1 AS (" +
-                        "SELECT zc.ZONE_NAME, zc.ZONE_CODE, SUM(DEPARTMENT_CLOSING_NO + TAXPAYER_CLOSING_NO) AS col4 " +
-                        "FROM mis_dla_gst_lgl_1 11a " +
-                        "LEFT JOIN mis_gst_commcode cc ON 11a.COMM_CODE = cc.COMM_CODE " +
-                        "LEFT JOIN mis_gst_zonecode zc ON cc.ZONE_CODE = zc.ZONE_CODE " +
-                        "WHERE 11a.MM_YYYY = '" + prev_month_new + "' AND FORUM_CODE = 6 " +
-                        "GROUP BY zc.ZONE_CODE) " +
-                        "SELECT cte.ZONE_NAME, cte.ZONE_CODE, cte.col10, cte1.col4, ((cte.col10 / cte1.col4)) AS total_score " +
-                        "FROM cte " +
-                        "INNER JOIN cte1 ON cte.ZONE_CODE = cte1.ZONE_CODE";
+                String queryTotalPending = "WITH cte AS (\n" +
+                        "                        SELECT zc.ZONE_NAME, zc.ZONE_CODE, SUM(DEPARTMENT_DISPOSAL_NO + TAXPAYER_DISPOSAL_NO) AS col10 \n" +
+                        "                        FROM mis_dla_gst_lgl_1 11a \n" +
+                        "                        LEFT JOIN mis_gst_commcode cc ON 11a.COMM_CODE = cc.COMM_CODE \n" +
+                        "                        LEFT JOIN mis_gst_zonecode zc ON cc.ZONE_CODE = zc.ZONE_CODE \n" +
+                        "                        WHERE 11a.MM_YYYY = '"+ month_date+"' AND FORUM_CODE = 6 \n" +
+                        "                        GROUP BY zc.ZONE_CODE), \n" +
+                        "                        cte1 AS (\n" +
+                        "                        SELECT zc.ZONE_NAME, zc.ZONE_CODE, SUM(DEPARTMENT_CLOSING_NO + TAXPAYER_CLOSING_NO) AS col4 \n" +
+                        "                        FROM mis_dla_gst_lgl_1 11a \n" +
+                        "                        LEFT JOIN mis_gst_commcode cc ON 11a.COMM_CODE = cc.COMM_CODE \n" +
+                        "                        LEFT JOIN mis_gst_zonecode zc ON cc.ZONE_CODE = zc.ZONE_CODE \n" +
+                        "                        WHERE 11a.MM_YYYY = '"+ prev_month_new+"' AND FORUM_CODE = 6 \n" +
+                        "                        GROUP BY zc.ZONE_CODE) \n" +
+                        "                        SELECT cte.ZONE_NAME, cte.ZONE_CODE, cte.col10, cte1.col4, ((cte.col10 / cte1.col4)) AS total_score \n" +
+                        "                        FROM cte \n" +
+                        "                        INNER JOIN cte1 ON cte.ZONE_CODE = cte1.ZONE_CODE\n" +
+                        "                        order by total_score desc;";
                 ResultSet rsTotalPending = GetExecutionSQL.getResult(queryTotalPending);
 
                 while (rsTotalPending.next()) {
                     String ra=RelevantAspect.Gst11A_RA;
                     String zoneName = rsTotalPending.getString("ZONE_NAME");
                     String zoneCode = rsTotalPending.getString("ZONE_CODE");
+                    double t_score = rsTotalPending.getDouble("total_score") * 100;
                     String commname = "ALL";
                     double col10 = rsTotalPending.getDouble("col10");
                     double col4 = rsTotalPending.getDouble("col4");
                     int Zonal_rank = 0;
                     String gst = "no";
-                    int way_to_grade = 0;
                     int insentavization = 0;
                     int sub_parameter_weighted_average = 0;
                     String absval=String.valueOf(col10)+"/"+String.valueOf(col4);
-                    total = (col4 != 0) ? (((col10 * 100) / col4) ) : 0;
+                    //total = (col4 != 0) ? (((col10 * 100) / col4) ) : 0;
 
-                    rank = score.marks11a(total,col10);
-                    String formattedTotal = String.format("%.2f", total);
+                    //rank = score.marks11a(total,col10);
+                    String formattedTotal = String.format("%.2f", t_score);
                     double totalScore = Double.parseDouble(formattedTotal);
+                    int way_to_grade =score.marks11a(totalScore,col10);
                     gsta = new GST4A(zoneName, commname, totalScore,absval,zoneCode,ra,
                             Zonal_rank,gst,way_to_grade,insentavization,sub_parameter_weighted_average);
                     allGstaList.add(gsta);
@@ -5440,22 +5442,22 @@ public class RetriveCbicDetailsController {
             }else if (type.equalsIgnoreCase("commissary")) { //gst 11a
                 String prev_month_new = DateCalculate.getPreviousMonth(month_date);
 
-                String queryTotalPending = "WITH cte AS ( " +
-                        "SELECT zc.ZONE_NAME, zc.ZONE_CODE, cc.COMM_NAME, cc.COMM_CODE, " +
-                        "(DEPARTMENT_DISPOSAL_NO + TAXPAYER_DISPOSAL_NO) AS col10 " +
-                        "FROM mis_dla_gst_lgl_1 AS 11a " +
-                        "LEFT JOIN mis_gst_commcode AS cc ON 11a.COMM_CODE = cc.COMM_CODE " +
-                        "LEFT JOIN mis_gst_zonecode AS zc ON cc.ZONE_CODE = zc.ZONE_CODE " +
-                        "WHERE 11a.MM_YYYY = '" + month_date + "' AND FORUM_CODE = 6 AND cc.ZONE_CODE = '" + zone_code + "'),cte1 AS ( " +
-                        "SELECT zc.ZONE_NAME, zc.ZONE_CODE, cc.COMM_NAME, cc.COMM_CODE, " +
-                        "(DEPARTMENT_CLOSING_NO + TAXPAYER_CLOSING_NO) AS col4 " +
-                        "FROM mis_dla_gst_lgl_1 AS 11a " +
-                        "LEFT JOIN mis_gst_commcode AS cc ON 11a.COMM_CODE = cc.COMM_CODE " +
-                        "LEFT JOIN mis_gst_zonecode AS zc ON cc.ZONE_CODE = zc.ZONE_CODE WHERE " +
-                        "11a.MM_YYYY = '" + month_date + "' AND FORUM_CODE = 6 AND cc.ZONE_CODE = '" + zone_code + "') " +
-                        "SELECT cte.ZONE_NAME,cte.ZONE_CODE,cte.COMM_NAME,cte.COMM_CODE,cte.col10,cte1.col4, " +
-                        "((cte.col10 / cte1.col4) ) AS total_score FROM cte " +
-                        "INNER JOIN cte1 ON cte.COMM_CODE = cte1.COMM_CODE";
+                String queryTotalPending = "WITH cte AS (SELECT zc.ZONE_NAME, zc.ZONE_CODE, cc.COMM_NAME, cc.COMM_CODE, \n" +
+                        "                        (DEPARTMENT_DISPOSAL_NO + TAXPAYER_DISPOSAL_NO) AS col10 \n" +
+                        "                        FROM mis_dla_gst_lgl_1 AS 11a \n" +
+                        "                        LEFT JOIN mis_gst_commcode AS cc ON 11a.COMM_CODE = cc.COMM_CODE \n" +
+                        "                        LEFT JOIN mis_gst_zonecode AS zc ON cc.ZONE_CODE = zc.ZONE_CODE \n" +
+                        "                        WHERE 11a.MM_YYYY = '"+ month_date+"' AND FORUM_CODE = 6 and zc.ZONE_CODE = '" + zone_code + "' ),cte1 AS ( \n" +
+                        "                        SELECT zc.ZONE_NAME, zc.ZONE_CODE, cc.COMM_NAME, cc.COMM_CODE, \n" +
+                        "                        (DEPARTMENT_CLOSING_NO + TAXPAYER_CLOSING_NO) AS col4 \n" +
+                        "                        FROM mis_dla_gst_lgl_1 AS 11a \n" +
+                        "                        LEFT JOIN mis_gst_commcode AS cc ON 11a.COMM_CODE = cc.COMM_CODE \n" +
+                        "                        LEFT JOIN mis_gst_zonecode AS zc ON cc.ZONE_CODE = zc.ZONE_CODE WHERE \n" +
+                        "                        11a.MM_YYYY = '"+ prev_month_new+"' AND FORUM_CODE = 6 and zc.ZONE_CODE = '" + zone_code + "') \n" +
+                        "                        SELECT cte.ZONE_NAME,cte.ZONE_CODE,cte.COMM_NAME,cte.COMM_CODE,cte.col10,cte1.col4, \n" +
+                        "                        ((cte.col10 / cte1.col4) ) AS total_score FROM cte \n" +
+                        "                        INNER JOIN cte1 ON cte.COMM_CODE = cte1.COMM_CODE\n" +
+                        "                        order by total_score desc;";
 
 
                 ResultSet rsTotalPending = GetExecutionSQL.getResult(queryTotalPending);
@@ -5465,11 +5467,11 @@ public class RetriveCbicDetailsController {
                     String zoneName = rsTotalPending.getString("ZONE_NAME");
                     String zoneCode = rsTotalPending.getString("ZONE_CODE");
                     String commname = rsTotalPending.getString("COMM_NAME");
+                    double t_score = rsTotalPending.getDouble("total_score") * 100;
                     double col10 = rsTotalPending.getDouble("col10");
                     double col4 = rsTotalPending.getDouble("col4");
                     int Zonal_rank = 0;
                     String gst = "no";
-                    int way_to_grade = 0;
                     int insentavization = 0;
                     int sub_parameter_weighted_average = 0;
                     total = (col4 != 0) ? ((col10 / col4)) : 0;
@@ -5479,8 +5481,9 @@ public class RetriveCbicDetailsController {
                     total = (col4 != 0) ? (col10 * 100 / col4) : 0;
 
                     rank = score.marks11a(total, col10);
-                    String formattedTotal = String.format("%.2f", total);
+                    String formattedTotal = String.format("%.2f", t_score);
                     double totalScore = Double.parseDouble(formattedTotal);
+                    int way_to_grade =score.marks11a(totalScore, col10);
                     gsta = new GST4A(rsTotalPending.getString("ZONE_NAME"), commname, totalScore,absval,zoneCode,ra,
                             Zonal_rank,gst,way_to_grade,insentavization,sub_parameter_weighted_average);
                     allGstaList.add(gsta);
@@ -5488,23 +5491,22 @@ public class RetriveCbicDetailsController {
             }else if (type.equalsIgnoreCase("all_commissary")) { //gst 11a
                 String prev_month_new = DateCalculate.getPreviousMonth(month_date);
 
-                String queryTotalPending = "WITH cte AS ( " +
-                        "SELECT zc.ZONE_NAME, zc.ZONE_CODE, cc.COMM_NAME, cc.COMM_CODE, " +
-                        "(DEPARTMENT_DISPOSAL_NO + TAXPAYER_DISPOSAL_NO) AS col10 " +
-                        "FROM mis_dla_gst_lgl_1 AS 11a " +
-                        "LEFT JOIN mis_gst_commcode AS cc ON 11a.COMM_CODE = cc.COMM_CODE " +
-                        "LEFT JOIN mis_gst_zonecode AS zc ON cc.ZONE_CODE = zc.ZONE_CODE " +
-                        "WHERE 11a.MM_YYYY = '" + month_date + "' AND FORUM_CODE = 6 ),cte1 AS ( " +
-                        "SELECT zc.ZONE_NAME, zc.ZONE_CODE, cc.COMM_NAME, cc.COMM_CODE, " +
-                        "(DEPARTMENT_CLOSING_NO + TAXPAYER_CLOSING_NO) AS col4 " +
-                        "FROM mis_dla_gst_lgl_1 AS 11a " +
-                        "LEFT JOIN mis_gst_commcode AS cc ON 11a.COMM_CODE = cc.COMM_CODE " +
-                        "LEFT JOIN mis_gst_zonecode AS zc ON cc.ZONE_CODE = zc.ZONE_CODE WHERE " +
-                        "11a.MM_YYYY = '" + month_date + "' AND FORUM_CODE = 6 ) " +
-                        "SELECT cte.ZONE_NAME,cte.ZONE_CODE,cte.COMM_NAME,cte.COMM_CODE,cte.col10,cte1.col4, " +
-                        "((cte.col10 / cte1.col4) ) AS total_score FROM cte " +
-                        "INNER JOIN cte1 ON cte.COMM_CODE = cte1.COMM_CODE";
-
+                String queryTotalPending = "WITH cte AS (SELECT zc.ZONE_NAME, zc.ZONE_CODE, cc.COMM_NAME, cc.COMM_CODE, \n" +
+                        "                        (DEPARTMENT_DISPOSAL_NO + TAXPAYER_DISPOSAL_NO) AS col10 \n" +
+                        "                        FROM mis_dla_gst_lgl_1 AS 11a \n" +
+                        "                        LEFT JOIN mis_gst_commcode AS cc ON 11a.COMM_CODE = cc.COMM_CODE \n" +
+                        "                        LEFT JOIN mis_gst_zonecode AS zc ON cc.ZONE_CODE = zc.ZONE_CODE \n" +
+                        "                        WHERE 11a.MM_YYYY = '"+ month_date+"' AND FORUM_CODE = 6 ),cte1 AS ( \n" +
+                        "                        SELECT zc.ZONE_NAME, zc.ZONE_CODE, cc.COMM_NAME, cc.COMM_CODE, \n" +
+                        "                        (DEPARTMENT_CLOSING_NO + TAXPAYER_CLOSING_NO) AS col4 \n" +
+                        "                        FROM mis_dla_gst_lgl_1 AS 11a \n" +
+                        "                        LEFT JOIN mis_gst_commcode AS cc ON 11a.COMM_CODE = cc.COMM_CODE \n" +
+                        "                        LEFT JOIN mis_gst_zonecode AS zc ON cc.ZONE_CODE = zc.ZONE_CODE WHERE \n" +
+                        "                        11a.MM_YYYY = '"+ prev_month_new+"' AND FORUM_CODE = 6 ) \n" +
+                        "                        SELECT cte.ZONE_NAME,cte.ZONE_CODE,cte.COMM_NAME,cte.COMM_CODE,cte.col10,cte1.col4, \n" +
+                        "                        ((cte.col10 / cte1.col4) ) AS total_score FROM cte \n" +
+                        "                        INNER JOIN cte1 ON cte.COMM_CODE = cte1.COMM_CODE\n" +
+                        "                        order by total_score desc;";
 
                 ResultSet rsTotalPending = GetExecutionSQL.getResult(queryTotalPending);
 
@@ -5513,11 +5515,11 @@ public class RetriveCbicDetailsController {
                     String zoneName = rsTotalPending.getString("ZONE_NAME");
                     String zoneCode = rsTotalPending.getString("ZONE_CODE");
                     String commname = rsTotalPending.getString("COMM_NAME");
+                    double t_score = rsTotalPending.getDouble("total_score") * 100;
                     double col10 = rsTotalPending.getDouble("col10");
                     double col4 = rsTotalPending.getDouble("col4");
                     int Zonal_rank = 0;
                     String gst = "no";
-                    int way_to_grade = 0;
                     int insentavization = 0;
                     int sub_parameter_weighted_average = 0;
                     total = (col4 != 0) ? ((col10 * 100 / col4)) : 0;
@@ -5527,8 +5529,9 @@ public class RetriveCbicDetailsController {
                     total = (col4 != 0) ? (col10 / col4) : 0;
 
                     rank = score.marks11a(total, col10);
-                    String formattedTotal = String.format("%.2f", total);
+                    String formattedTotal = String.format("%.2f", t_score);
                     double totalScore = Double.parseDouble(formattedTotal);
+                    int way_to_grade = score.marks11a(totalScore, col10);
                     gsta = new GST4A(rsTotalPending.getString("ZONE_NAME"), commname, totalScore,absval,zoneCode,ra,
                             Zonal_rank,gst,way_to_grade,insentavization,sub_parameter_weighted_average);
                     allGstaList.add(gsta);
