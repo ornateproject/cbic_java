95c4197ea0e573640fe855d3059f0967c486be72
rakeshshit <iamrakeshit60@gmail.com>
2024-08-22 19:08:14 +0530

    updated

--- src/main/java/com/ornates/cbic/controller/TotalScoreController.java
+++ src/main/java/com/ornates/cbic/controller/TotalScoreController.java
@@ -554,7 +554,7 @@ public class TotalScoreController {
 					String formattedTotal = String.format("%.2f", total);
 					double total_score = Double.parseDouble(formattedTotal);
 					Integer way_to_grade =score.marks2(total_score);
-					double sub_parameter_weighted_average = way_to_grade;
+					double sub_parameter_weighted_average = way_to_grade * 0.5;
 					totalScore = new TotalScore(zoneName, commName,zone_code, total_score, absval, Zonal_rank, gst,ra,way_to_grade,insentavization,sub_parameter_weighted_average);
 					allGstaList.add(totalScore);
 				}
@@ -2563,7 +2563,7 @@ public class TotalScoreController {
 					String formattedTotal = String.format("%.2f", tScore);
 					double total_score = Double.parseDouble(formattedTotal);
 					int way_to_grade = score.marks7(total_score);
-					double sub_parameter_weighted_average = way_to_grade;
+					double sub_parameter_weighted_average = way_to_grade * 0.5;
 					//System.out.println(way_to_grade);
 					totalScore = new TotalScore(zoneName, commName, zone_code, total_score, absval, Zonal_rank, gst,ra,way_to_grade,insentavization,sub_parameter_weighted_average);
 					allGstaList.add(totalScore);
