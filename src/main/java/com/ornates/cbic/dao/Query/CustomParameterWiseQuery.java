9bb3f3f13ac13ccc5c37d0eee7fdeef804bcd945
rakeshshit <iamrakeshit60@gmail.com>
2024-10-20 21:47:05 +0530

    updated

--- src/main/java/com/ornates/cbic/dao/Query/CustomParameterWiseQuery.java
+++ src/main/java/com/ornates/cbic/dao/Query/CustomParameterWiseQuery.java
@@ -1,4 +1,114 @@
 package com.ornates.cbic.dao.Query;
 import com.ornates.cbic.service.DateCalculate;
 public class CustomParameterWiseQuery {
+    // ***********************************CUS 1 parameter wise(Timely payment of Refund) *****************************************************************
+    //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+    // this query will show all zone || 1no url
+    public String QueryFor_TimelyPaymentOfRefunds_1_ZoneWise(String month_date){
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus1 = "";
+        return query_assessment_cus1;
+    }
+    // for 2no url , all india rank will show in this query
+    public String QueryFor_TimelyPaymentOfRefunds_1_ParticularZoneWise(String month_date, String zone_code){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus1 = "";
+        return query_assessment_cus1;
+    }
+    //  for perticular subparameter wise ||  3 no url
+    public String QueryFor_TimelyPaymentOfRefunds_1_ParticularSubparameterWise(String month_date, String zone_code){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus1 = "";
+        return query_assessment_cus1;
+    }
+    //  for all commissary wise || for 4no url
+    public String QueryFor_TimelyPaymentOfRefunds_1_AllCommissary(String month_date){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus1 = "";
+        return query_assessment_cus1;
+    }
+    //  for perticular commissonary in subparameter || for 5no url
+    public String QueryFor_TimelyPaymentOfRefunds_1_ParticularCommissonaryInSubparameter(String month_date, String zone_code,String come_name){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus1 = "";
+        return query_assessment_cus1;
+    }
+
+    // ***********************************CUS 2 parameter wise(Management Of Export Obligation - EPGC) *****************************************************************
+    //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+    // this query will show all zone || 1no url
+    public String QueryFor_ManagementOfExportObligation_EPGC_2_ZoneWise(String month_date){
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
+    // for 2no url , all india rank will show in this query
+    public String QueryFor_ManagementOfExportObligation_EPGC_2_ParticularZoneWise(String month_date, String zone_code){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
+    //  for perticular subparameter wise ||  3 no url
+    public String QueryFor_ManagementOfExportObligation_EPGC_2_ParticularSubparameterWise(String month_date, String zone_code){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
+    //  for all commissary wise || for 4no url
+    public String QueryFor_ManagementOfExportObligation_EPGC_2_AllCommissary(String month_date){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
+    //  for perticular commissonary in subparameter || for 5no url
+    public String QueryFor_ManagementOfExportObligation_EPGC_2_ParticularCommissonaryInSubparameter(String month_date, String zone_code,String come_name){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
+
+    // ***********************************CUS 3 parameter wise(Management Of Export Obligation - AA) *****************************************************************
+    //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+    // this query will show all zone || 1no url
+    public String QueryFor_ManagementOfExportObligation_AA_3_ZoneWise(String month_date){
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
+    // for 2no url , all india rank will show in this query
+    public String QueryFor_ManagementOfExportObligation_AA_3_ParticularZoneWise(String month_date, String zone_code){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
+    //  for perticular subparameter wise ||  3 no url
+    public String QueryFor_ManagementOfExportObligation_AA_3_ParticularSubparameterWise(String month_date, String zone_code){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
+    //  for all commissary wise || for 4no url
+    public String QueryFor_ManagementOfExportObligation_AA_3_AllCommissary(String month_date){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
+    //  for perticular commissonary in subparameter || for 5no url
+    public String QueryFor_ManagementOfExportObligation_AA_3_ParticularCommissonaryInSubparameter(String month_date, String zone_code,String come_name){
+        //              '" + month_date + "'	 '" + prev_month_new + "'	'" + zone_code + "'		'" + come_name + "' 	'" + next_month_new + "'
+        String prev_month_new = DateCalculate.getPreviousMonth(month_date);
+        String query_assessment_cus2 = "";
+        return query_assessment_cus2;
+    }
 }
