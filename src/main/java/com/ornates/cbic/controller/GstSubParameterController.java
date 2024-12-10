957930982e52cf9fd58a482b70324e12ecbbd1e1
ashish <ashishkalyan6@gmail.com>
2024-12-10 14:11:03 +0530

    gst 3b has been updated

--- src/main/java/com/ornates/cbic/controller/GstSubParameterController.java
+++ src/main/java/com/ornates/cbic/controller/GstSubParameterController.java
@@ -1365,15 +1365,43 @@ public class GstSubParameterController {
         try {
             if (type.equalsIgnoreCase("zone")) {
                 String queryGst14aa =new GstSubParameterWiseQuery().QueryFor_gst3b_ZoneWise(month_date);
-                ResultSet rsGst14aa = GetExecutionSQL.getResult(queryGst14aa);
-                allGstaList.addAll(gstSubParameterService.gst3bZone(rsGst14aa));
+                PreparedStatement psGst14aa=con.prepareStatement(queryGst14aa);
+                //Result Set
+                ResultSet rsGst14aa= psGst14aa.executeQuery();
+                while(rsGst14aa.next() ) {
+                    String commname = "ALL";
+                    String ra = RelevantAspect.Gst3B_RA;
+                    String zoneName = rsGst14aa.getString("ZONE_NAME");
+                    String zoneCode = rsGst14aa.getString("ZONE_CODE");
+                    String absval = rsGst14aa.getString("absval");
+                    double t_score = rsGst14aa.getDouble("score_of_parameter");
+                    median = rsGst14aa.getDouble("median_numerator_3b");
+                    Double numerator_3b = rsGst14aa.getDouble("numerator_3b");
+
+
+                    String formattedTotal = String.format("%.2f", t_score);
+                    double totalScore = Double.parseDouble(formattedTotal);
+                    int way_to_grade = score.marks3b(totalScore);
+                    int insentavization = score.marks3b(totalScore);
+
+                    if (numerator_3b > median && way_to_grade < 10) {
+                        insentavization += 1;
+                    }
+                    int Zonal_rank = 0;
+                    String gst = "no";
+
+                    Double sub_parameter_weighted_average = insentavization * 0.5;
+                    gsta = new GST4A(zoneName, commname, totalScore, absval, zoneCode, ra,
+                            Zonal_rank, gst, way_to_grade, insentavization, sub_parameter_weighted_average);
+                    allGstaList.add(gsta);
+                }
             }else if (type.equalsIgnoreCase("commissary")) {
                 String prev_month_new =DateCalculate.getPreviousMonth(month_date);
                 // Query string
                 String queryGst14aa= new GstSubParameterWiseQuery().QueryFor_gst3b_CommissonaryWise(month_date,zone_code);
                 PreparedStatement psGst14aa=con.prepareStatement(queryGst14aa);
                 ResultSet rsGst14aa= psGst14aa.executeQuery();
-                allGstaList.addAll(gstSubParameterService.gst3bZone(rsGst14aa));
+                //allGstaList.addAll(gstSubParameterService.gst3bZone(rsGst14aa));
                 while(rsGst14aa.next() ) {
                     String commname=rsGst14aa.getString("COMM_NAME");
                     String ra=RelevantAspect.Gst3B_RA;
--- src/main/java/com/ornates/cbic/service/GstSubParameterService.java
+++ src/main/java/com/ornates/cbic/service/GstSubParameterService.java
@@ -15,34 +15,34 @@ public class GstSubParameterService {
     GST4A gsta = null;
 
     // *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=GST 3B zone wise *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-    public List<GST4A> gst3bZone(ResultSet rsGst14aa) throws SQLException {
-        List<GST4A> allGstaList = new ArrayList<>();
-
-        while (rsGst14aa.next()) {
-            String commname = "All";
-            String ra = RelevantAspect.Gst3B_RA;
-            String zoneName = rsGst14aa.getString("ZONE_NAME");
-            String zoneCode = rsGst14aa.getString("ZONE_CODE");
-            Double col22 = rsGst14aa.getDouble("col22");
-            Double col14 = rsGst14aa.getDouble("col14");
-
-            double totalScore = (col14 != 0) ? (col22 / col14) * 100 : 0.00;
-            String formattedTotal = String.format("%.2f", totalScore);
-            totalScore = Double.parseDouble(formattedTotal);
-
-            String absval = String.valueOf(col22) + "/" + String.valueOf(col14);
-            int way_to_grade = score.marks3b(totalScore);
-
-            int Zonal_rank = 0;
-            String gst = "GST 3B";
-
-            Integer insentavization = 0;
-            double sub_parameter_weighted_average = 0.00;
-            gsta = new GST4A(zoneName, commname, totalScore, absval, zoneCode, ra, Zonal_rank, gst, way_to_grade, insentavization, sub_parameter_weighted_average);
-            allGstaList.add(gsta);
-        }
-        return allGstaList;
-    }
+//    public List<GST4A> gst3bZone(ResultSet rsGst14aa) throws SQLException {
+//        List<GST4A> allGstaList = new ArrayList<>();
+//
+//        while (rsGst14aa.next()) {
+//            String commname = "All";
+//            String ra = RelevantAspect.Gst3B_RA;
+//            String zoneName = rsGst14aa.getString("ZONE_NAME");
+//            String zoneCode = rsGst14aa.getString("ZONE_CODE");
+//            Double col22 = rsGst14aa.getDouble("col22");
+//            Double col14 = rsGst14aa.getDouble("col14");
+//
+//            double totalScore = (col14 != 0) ? (col22 / col14) * 100 : 0.00;
+//            String formattedTotal = String.format("%.2f", totalScore);
+//            totalScore = Double.parseDouble(formattedTotal);
+//
+//            String absval = String.valueOf(col22) + "/" + String.valueOf(col14);
+//            int way_to_grade = score.marks3b(totalScore);
+//
+//            int Zonal_rank = 0;
+//            String gst = "GST 3B";
+//
+//            Integer insentavization = 0;
+//            double sub_parameter_weighted_average = 0.00;
+//            gsta = new GST4A(zoneName, commname, totalScore, absval, zoneCode, ra, Zonal_rank, gst, way_to_grade, insentavization, sub_parameter_weighted_average);
+//            allGstaList.add(gsta);
+//        }
+//        return allGstaList;
+//    }
         // *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*= GST 4D zone wise *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
 
     public List<GST4A> gst4dZone(ResultSet rsGst14aa) throws SQLException {
